d5a31481d7d9d46fe719fd77e2780c3f6e6074a1
chandan chakma <cchakma19@gmail.com>
2022-07-16 07:15:19 -0700

    post item

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+require('dotenv').config()
+
 const app = express();
 
+app.use(cors())
+app.use(express.json())
 
 
 // user store
 // password 5xg4idBmIN95YwVN
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster5.xu2ob.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+
+        const storeCollection = client.db("store").collection("product");
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = storeCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+        app.post('/product', async (req, res) => {
+            const newUser = req.body;
+            const result = await storeCollection.insertOne(newUser);
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('hello girl')
