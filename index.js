d9967aa2dadcffc29b6615c093361023c76d5944
chandan chakma <cchakma19@gmail.com>
2022-07-26 20:02:21 -0700

    add and delete product

--- index.js
+++ index.js
@@ -43,6 +43,12 @@ async function run() {
             const result = await storeCollection.insertOne(newProduct);
             res.send(result);
         })
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await storeCollection.deleteOne(query)
+            res.send(result);
+        })
 
     }
     finally {
